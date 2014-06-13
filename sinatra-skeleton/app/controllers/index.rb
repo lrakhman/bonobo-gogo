df811587696b7f015881748ce7726a2f24984322
Lana R. & Monica Cho <pair+lrakhman.monicadev@devbootcamp.com>
2014-06-13 12:49:20 -0500

    routes framed

---
+++ sinatra-skeleton/app/controllers/index.rb
@@ -0,0 +1,40 @@
+
+get '/' do
+  erb :index
+end
+
+get 'users/:user_id/surveys/new' do
+
+end
+
+get 'users/:user_id' do
+
+end
+
+get 'surveys/:survey_id/detail' do
+
+end
+
+get 'surveys/:survey_id' do
+
+end
+
+get 'logout' do
+
+end
+
+post 'login' do
+
+end
+
+post 'users/new' do
+
+end
+
+post 'users/:user_id/surveys/new' do
+
+end
+
+post 'surveys/:survey_id' do
+
+end
