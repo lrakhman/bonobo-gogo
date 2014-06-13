77e1c416aefea56cb51a1d8751592f9946a2e92a
Lana R. & Monica Cho <pair+lrakhman.monicadev@devbootcamp.com>
2014-06-13 16:23:09 -0500

    layout works

--- sinatra-skeleton/app/controllers/users.rb
+++ sinatra-skeleton/app/controllers/users.rb
@@ -1,15 +1,21 @@
-get 'users/:user_id/surveys/new' do
+get '/users/:user_id/surveys/new' do
 
 end
 
-get 'users/:user_id' do
+get '/users/:user_id' do
 
 end
 
-post 'users/new' do
-
+post '/users' do
+  @user = User.new(username: params[:username], password: params[:password])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :index
+  end
 end
 
-post 'users/:user_id/surveys/new' do
+post '/users/:user_id/surveys/new' do
 
 end
