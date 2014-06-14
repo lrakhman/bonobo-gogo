2bb5e343dee8c53b69fdd33a39ea9485b722355b
Xander Glassman & Zac Mitton <pair+xanderglassman.zmitton@devbootcamp.com>
2014-06-14 14:08:26 -0500

    fixed up a bit from that error

--- sinatra-skeleton/app/controllers/users.rb
+++ sinatra-skeleton/app/controllers/users.rb
@@ -33,7 +33,7 @@ post '/users/:user_id/surveys/new' do
   if session[:user_id]
     questions = JSON.parse(params[:json_form])
 
-    survey = Survey.create(title: params[:title] , description: params[:description])
+    survey = Survey.create(title: params[:title] , description: params[:description], survey_creator_id: session[:user_id])
     questions.each do |question|
       question_obj = Question.create(survey_id: survey.id, text: question['text'])
       #puts question["choices"].length
@@ -46,4 +46,15 @@ post '/users/:user_id/surveys/new' do
   else
     redirect "/"
   end
+end
+
+post '/users/:user_id' do
+# params['uploaded_photo'][:filename]
+
+ # File.open("uploads/" + "#{params[:user_id]}" do |f|
+ #    f.write(params['uploaded_photo'][:tempfile].read)
+ #    puts(params['uploaded_photo'])
+ #  end
+
+ #redirect "/users/#{params[:user_id]}"
 end
