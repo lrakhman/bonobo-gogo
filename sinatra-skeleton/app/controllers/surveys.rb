28be5f6459d38c4dfe3b87244736e205fe235802
MonicaDev <monicacho21@gmail.com>
2014-06-14 16:14:45 -0500

    created a route for surveys responses

--- sinatra-skeleton/app/controllers/surveys.rb
+++ sinatra-skeleton/app/controllers/surveys.rb
@@ -1,5 +1,8 @@
 
-get 'surveys/:survey_id/stats' do
+get '/surveys/:survey_response_survey_id/stats' do
+
+
+	erb :stats
 
 end
 
@@ -17,13 +20,11 @@ post '/surveys/:survey_id' do
 
 	survey_taken = SurveyResponse.create(survey_id: params[:survey_id] ,user_id: session[:user_id] )
 	survey = Survey.find(params[:survey_id])
-
-
 	survey.questions.each do |question|
 
 		QuestionResponse.create(choice_id: params[question.id].to_i, survey_response_id: survey_taken.id)
 	end
-	
+
 	redirect "/users/#{session[:user_id]}"
 end
 
